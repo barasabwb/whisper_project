a20a2793836b5ec833d408396c298e2b42723a25
Brian Barasa <barasabwb17@gmail.com>
2023-07-14 21:01:19 +0300

    updates

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-const ROOT = 'https://whisperproject.space/whisper/';
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+const ROOT = 'https://whisperproject.space/';
 //const BASE = __DIR__;
 
 const BASE = '';
